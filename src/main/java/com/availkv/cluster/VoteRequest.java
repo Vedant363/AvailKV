e6634f4518f4ba43427fada1567ded8030abd59e
Vedant363 <vedant.ghumade363@gmail.com>
2026-06-09 17:24:56 +0530

    add logsize variable, getter, setter in voterequest

--- src/main/java/com/availkv/cluster/VoteRequest.java
+++ src/main/java/com/availkv/cluster/VoteRequest.java
@@ -3,16 +3,20 @@ package com.availkv.cluster;
 public class VoteRequest {
     private String candidateId;
     private int term;
+    private int logSize;
 
     public VoteRequest() {}
 
-    public VoteRequest(String candidateId, int term) {
+    public VoteRequest(String candidateId, int term, int logSize) {
         this.candidateId = candidateId;
         this.term = term;
+        this.logSize = logSize;
     }
 
     public String getCandidateId() { return candidateId; }
     public void setCandidateId(String candidateId) { this.candidateId = candidateId; }
     public int getTerm() { return term; }
     public void setTerm(int term) { this.term = term; }
+    public int getLogSize() { return logSize; }
+    public void setLogSize(int logSize) { this.logSize = logSize; }
 }
